dc663dc1ad5e50f54d9c1897957da00cc4588863
rakibul-wdp <rakibul.wdp@gmail.com>
2022-10-30 20:24:05 +0600

    define folder and storage

--- utilities/singleUploader.js
+++ utilities/singleUploader.js
@@ -1,10 +1,33 @@
+// external imports
+const multer = require('multer');
+const path = require('path');
+const createError = require('http-errors');
+
 function uploader(
   subfolder_path,
   allowed_file_types,
   max_file_size,
   error_msg
 ) {
-  // make upload object
+  // File upload folder
+  const UPLOADS_FOLDER = `${__dirname}/../public/uploads/${subfolder_path}`;
+
+  // define the storage
+  const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, UPLOADS_FOLDER);
+    },
+    filename: (req, file, cb) => {
+      cb(null, UPLOADS_FOLDER);
+    },
+    filename: (req, file, cb) => {
+      const fileExt = path.extname(file.originalname);
+      const fileName = file.originalname.replace(fileExt, '').toLowerCase().split(' ').join('-') + '-' + Date.now();
+
+      cb(null, fileName + fileExt);
+    },
+  });
+
   return upload;
 }
 
