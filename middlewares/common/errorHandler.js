38fdcab6c2dbe6462a22c93c3b2348d30eba22f9
rakibul-wdp <rakibul.wdp@gmail.com>
2022-10-30 11:52:06 +0600

    create two default error handler

--- middlewares/common/errorHandler.js
+++ middlewares/common/errorHandler.js
@@ -3,9 +3,16 @@ const createError = require('http-errors');
 // 404 not found handler
 function notFoundHandler(req, res, next) {
   next(createError(404, 'Your requested content was not found'));
-}
+};
 
 // default error handler
 function errorHandler(err, req, res, next) {
-  
+  res.render('error', {
+    title: 'Error page'
+  });
 };
+
+module.exports = {
+  notFoundHandler,
+  errorHandler,
+}
