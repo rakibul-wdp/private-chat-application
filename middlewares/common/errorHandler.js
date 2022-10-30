9f57abb45da0ced2a910f4ce74cc584ef6124f30
rakibul-wdp <rakibul.wdp@gmail.com>
2022-10-30 11:45:03 +0600

    create not found handler

---
+++ middlewares/common/errorHandler.js
@@ -0,0 +1,11 @@
+const createError = require('http-errors');
+
+// 404 not found handler
+function notFoundHandler(req, res, next) {
+  next(createError(404, 'Your requested content was not found'));
+}
+
+// default error handler
+function errorHandler(err, req, res, next) {
+  
+};
