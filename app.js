9d16170629b473bfe8bb3a935e43105478803fc0
rakibul-wdp <rakibul.wdp@gmail.com>
2022-10-30 15:29:29 +0600

    create basic users router

--- app.js
+++ app.js
@@ -35,8 +35,8 @@ app.use(cookieParser(process.env.COOKIE_SECRET));
 
 // routing setup
 app.use('/', loginRouter);
-// app.use('/users', usersRouter);
-// app.use('/inbox', inboxRouter);
+app.use('/users', usersRouter);
+app.use('/inbox', inboxRouter);
 
 // 404 not found handler
 app.use(notFoundHandler);
