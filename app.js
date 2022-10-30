0124c566eccb71b377b944167a21289d3ced67e1
rakibul-wdp <rakibul.wdp@gmail.com>
2022-10-30 12:05:25 +0600

    ultimale error handle

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+// external imports
 const express = require('express');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const path = require('path');
 const cookieParser = require('cookie-parser');
+
+// internal imports
+const {notFoundHandler, errorHandler} = require('./middlewares/common/errorHandler');
 
 const app = express();
 dotenv.config();
@@ -30,7 +34,12 @@ app.use(cookieParser(process.env.COOKIE_SECRET));
 
 // routing setup
 
-// error handling
+// 404 not found handler
+app.use(notFoundHandler);
+
+// common error handler
+app.use(errorHandler);
+
 
 app.listen(process.env.PORT, () => {
   console.log(`app listening to port ${process.env.PORT}`);
