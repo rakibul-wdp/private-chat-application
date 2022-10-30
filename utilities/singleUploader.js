8550a34655276456460d79c6fe2d64cb1a16bb1d
rakibul-wdp <rakibul.wdp@gmail.com>
2022-10-30 20:49:27 +0600

    create upload object

--- utilities/singleUploader.js
+++ utilities/singleUploader.js
@@ -18,13 +18,25 @@ function uploader(
       cb(null, UPLOADS_FOLDER);
     },
     filename: (req, file, cb) => {
-      cb(null, UPLOADS_FOLDER);
-    },
-    filename: (req, file, cb) => {
       const fileExt = path.extname(file.originalname);
       const fileName = file.originalname.replace(fileExt, '').toLowerCase().split(' ').join('-') + '-' + Date.now();
 
       cb(null, fileName + fileExt);
+    },
+  });
+
+  // prepare the final multer upload object
+  const upload = multer({
+    storage: storage,
+    limits: {
+      fileSize: max_file_size,
+    },
+    fileFilter: (req, file, cb) => {
+      if (allowed_file_types.includes(file.mimetype)) {
+        cb(null, tru);
+      } else {
+        cb(createError(error_msg));
+      }
     },
   });
 
