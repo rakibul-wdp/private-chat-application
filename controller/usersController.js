c938bdfaf6dbf8b90b77e3b0e57d545c39d7bc95
rakibul-wdp <rakibul.wdp@gmail.com>
2022-11-03 04:52:23 +0600

    create try catch block for save user

--- controller/usersController.js
+++ controller/usersController.js
@@ -26,8 +26,25 @@ async function addUser(req, res, next) {
       password: hashedPassword,
     });
   }
+
+  // save user or send error
+  try {
+    const result = await newUser.save();
+    res.status(200).json({
+      message: 'User was added successfully...!!!',
+    });
+  } catch (err) {
+    res.status(500).json({
+      errors: {
+        common: {
+          msg: 'Unknown error occurred',
+        },
+      },
+    });
+  }
 }
 
 module.exports = {
   getUsers,
+  addUser,
 }
