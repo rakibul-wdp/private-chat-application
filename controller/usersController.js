396fbe6782b2d4cb0c2994959861314666cbeb45
rakibul-wdp <rakibul.wdp@gmail.com>
2022-11-08 04:47:06 +0600

    add remove functionality on user controller

--- controller/usersController.js
+++ controller/usersController.js
@@ -51,6 +51,33 @@ async function addUser(req, res, next) {
   }
 }
 
+// remove user
+async function removeUser(req, res, next) {
+  try {
+    const user = await User.findByIdAndDelete({
+      _id: req.params.id,
+    });
+
+    // remove user avatar if any
+    if (user.avatar) {
+      unlink(
+        path.join(__dirname, `/../public/uploads/avatars/${user.avatar}`),
+        (err) => {
+          if (err) console.log(err);
+        }
+      )
+    }
+  } catch (err) {
+    res.status(500).json({
+      errors: {
+        common: {
+          msg: 'Could not delete the user!',
+        }
+      }
+    })
+  }
+}
+
 module.exports = {
   getUsers,
   addUser,
