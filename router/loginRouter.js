f77f96f8287d0c932483285a2a2027ccc84067a4
rakibul-wdp <rakibul.wdp@gmail.com>
2022-11-09 11:57:50 +0600

    add some locals in loggind validator

--- router/loginRouter.js
+++ router/loginRouter.js
@@ -4,13 +4,17 @@ const express = require('express');
 // internal import
 const {getLogin, login} = require('../controller/loginController');
 const decorateHtmlResponse = require('../middlewares/common/decorateHtmlResponse');
+const { doLoginValidators, doLoginValidationHandler } = require('../middlewares/login/loginValidators');
 
 const router = express.Router();
 
+// set page title
+const page_title = 'Login';
+
 // login page
-router.get('/', decorateHtmlResponse('Login'), getLogin);
+router.get('/', decorateHtmlResponse(page_title), getLogin);
 
 // process login
-router.post('/', login);
+router.post('/', decorateHtmlResponse(page_title), doLoginValidators, doLoginValidationHandler, login);
 
 module.exports = router;
