ee938f04a414421b8f71f9041aa618966edbccf6
rakibul-wdp <rakibul.wdp@gmail.com>
2022-11-09 11:32:57 +0600

    create login validations handler

--- middlewares/login/loginValidators.js
+++ middlewares/login/loginValidators.js
@@ -9,6 +9,22 @@ const doLoginValidators = [
   check('password').isLength({min: 1}).withMessage('Password is required'),
 ];
 
+const doLoginValidationHandler = function (req, res, next) {
+  const errors = validationResult(req);
+  const mappedErrors = errors.mapped();
+  if (Object.keys(mappedErrors).length === 0) {
+    next();
+  } else {
+    res.render('index', {
+      data: {
+        username: req.body.username,
+      },
+      errors: mappedErrors,
+    });
+  }
+};
+
 module.exports = {
   doLoginValidators,
+  doLoginValidationHandler,
 }
