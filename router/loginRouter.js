f8356c78bf99c341e9100f09d8d70a952c4d3ea0
rakibul-wdp <rakibul.wdp@gmail.com>
2022-11-10 06:41:08 +0600

    ridirect login page

--- router/loginRouter.js
+++ router/loginRouter.js
@@ -3,6 +3,7 @@ const express = require('express');
 
 // internal import
 const {getLogin, login, logout} = require('../controller/loginController');
+const { redirectLoggedIn } = require('../middlewares/common/checkLogin');
 const decorateHtmlResponse = require('../middlewares/common/decorateHtmlResponse');
 const { doLoginValidators, doLoginValidationHandler } = require('../middlewares/login/loginValidators');
 
@@ -12,7 +13,7 @@ const router = express.Router();
 const page_title = 'Login';
 
 // login page
-router.get('/', decorateHtmlResponse(page_title), getLogin);
+router.get('/', decorateHtmlResponse(page_title), redirectLoggedIn, getLogin);
 
 // process login
 router.post('/', decorateHtmlResponse(page_title), doLoginValidators, doLoginValidationHandler, login);
