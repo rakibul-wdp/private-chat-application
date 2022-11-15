097c3402bc00afba62e049edc780c727867ffd09
rakibul-wdp <rakibul.wdp@gmail.com>
2022-11-16 05:14:41 +0600

    create require role function

--- router/usersRouter.js
+++ router/usersRouter.js
@@ -1,5 +1,6 @@
 // external imports
 const express = require('express');
+const { check } = require('express-validator');
 
 // internal import
 const {getUsers, addUser, removeUser} = require('../controller/usersController');
@@ -7,17 +8,17 @@ const decorateHtmlResponse = require('../middlewares/common/decorateHtmlResponse
 const avatarUpload = require('../middlewares/users/avatarUpload');
 const { addUserValidators, addUserValidationHandler } = require('../middlewares/users/userValidators');
 
-const  {checkLogin}  = require('../middlewares/common/checkLogin');
+const  {checkLogin, requireRole}  = require('../middlewares/common/checkLogin');
 
 const router = express.Router();
 
 // users page
-router.get('/', decorateHtmlResponse('Users'), checkLogin, getUsers);
+router.get('/', decorateHtmlResponse('Users'), checkLogin,requireRole(['admin']), getUsers);
 
 // add user
-router.post('/', checkLogin, avatarUpload, addUserValidators, addUserValidationHandler, addUser);
+router.post('/', checkLogin, requireRole(['admin']), avatarUpload, addUserValidators, addUserValidationHandler, addUser);
 
 // remove user
-router.delete('/:id', removeUser);
+router.delete('/:id', checkLogin, requireRole(['admin']), removeUser);
 
 module.exports = router;
